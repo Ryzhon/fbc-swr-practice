a5d9e25e48e1427d1f6bd0ab03747b59ec284c4d
Ryzhon <pekozono@email.com>
2024-02-15 10:32:02 +0900

    仕様をSWRに変更 理由：ローディング画面が白くなってしまう。

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { useState } from "react";
 import "./App.css";
+import useSWR from "swr";
 
 function App() {
+  const [status, setStatus] = useState("");
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
-
-  const [status, setStatus] = useState("");
-
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
-
-  return <>{status && <p>Status : {status}</p>}</>;
+  const fetcher = (url) =>
+    fetch(url, { headers })
+      .then((res) => res.json())
+      .then((json) => setStatus(json.description));
+  const { data, error, isLoading } = useSWR(url, fetcher);
+  console.log(data)
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+  return (
+    <>
+      {status && (
+        <p>
+          Status : {status}
+        </p>
+      )}
+    </>
+  );
 }
 
 export default App;
