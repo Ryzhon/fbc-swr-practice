839510a713ac25aefc7e8ec4973b182362dcd2b2
Ryzhon <ryuta.hokazono@gmail.com>
2024-02-19 07:51:09 +0900

    fix: statusのstate管理を削除 理由：管理するほど処理が書かれていないため

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import { useState } from "react";
 import "./App.css";
 import useSWR from "swr";
 
 function App() {
-  const [status, setStatus] = useState("");
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
   const fetcher = (url) =>
     fetch(url, { headers })
       .then((res) => res.json())
-      .then((json) => setStatus(json.description));
+      .then((json) => json.description);
   const { data, error, isLoading } = useSWR(url, fetcher);
-  console.log(data)
   if (error) return <div>Failed to load</div>;
   if (isLoading) return <div>Loading...</div>;
-  return (
-    <>
-      {status && (
-        <p>
-          Status : {status}
-        </p>
-      )}
-    </>
-  );
+  return <>{data && <p>Status : {data}</p>}</>;
 }
 
 export default App;
